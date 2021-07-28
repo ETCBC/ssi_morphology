aebe421a549e8a39197d983ba2303fb28ecb6864
Jisk Attema <j.attema@esciencecenter.nl>
2021-07-28 16:54:04 +0200

    Add loading/saving of the model

--- src/model.py
+++ src/model.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -9,6 +10,7 @@ class HebrewEncoder(nn.Module):
     def __init__(self, input_dim, hidden_dim):
         super(HebrewEncoder, self).__init__()
         self.hidden_dim = hidden_dim
+        self.input_dim = input_dim
 
         self.input_embeddings = nn.Embedding(input_dim, hidden_dim)
         self.gru = nn.GRU(hidden_dim, hidden_dim, num_layers=2)
@@ -28,6 +30,7 @@ class HebrewDecoder(nn.Module):
     def __init__(self, hidden_dim, output_dim):
         super(HebrewDecoder, self).__init__()
         self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
 
         self.output_embeddings = nn.Embedding(output_dim, hidden_dim)
         self.gru = nn.GRU(hidden_dim, hidden_dim, num_layers=2)
@@ -44,3 +47,33 @@ class HebrewDecoder(nn.Module):
 
     def initHidden(self):
         return torch.zeros(2, 1, self.hidden_dim, device=device)
+
+
+def save_encoder_decoder(encoder, decoder, path=None, filename="model.pt"):
+    if not path:
+        path = "."
+
+    state = {
+            "input_dim": encoder.input_dim,
+            "hidden_dim": encoder.hidden_dim,
+            "output_dim": decoder.output_dim,
+            "encoder": encoder.state_dict(),
+            "decoder": decoder.state_dict()
+            }
+
+    torch.save(state, os.path.join(path, filename))
+
+
+def load_encoder_decoder(encoder, decoder, path=None, filename="model.pt"):
+    if not path:
+        path = "."
+
+    state = torch.load(os.path.join(path, filename))
+
+    encoder = HebrewEncoder(state['input_dim'], state['hidden_dim'])
+    decoder = HebrewDecoder(state['hidden_dim'], state['output_dim'])
+
+    encoder.load_state_dict(state['encoder'])
+    decoder.load_state_dict(state['decoder'])
+
+    return encoder, decoder
